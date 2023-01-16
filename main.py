07344c94f8a32699a075aa40946915e280e3b640
Deni Irawan <denyirawan341@gmail.com>
2023-01-16 21:58:18 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# a itu variabel = adalah asignment dan 10 adalah value
+# jadi variabel adalah tempat menyimpan data
+
+# menaruh / assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+#pemanggilan pertama
+print("Nilai a =", a)
+print("Nilai x =", x)
+print("Nilai panjang =", panjang)
+
+# penamaan
+nilai_y = 15 #dengan underscore
+juta10 = 10000000 # dengan angka
+nilaiZ = 17.5 #ini boleh
+
+#pemanggilan kedua
+print("Nilai a =", a)
+a = 7
+print("Nilai a =", a)
+#assignment indirect
+b = a
+print("Nilai b =", b)
