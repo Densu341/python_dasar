87f75e0132d2ff9b460a0d33f8cf64587e99fc95
Deni Irawan <denyirawan341@gmail.com>
2023-01-17 22:31:47 +0700

    latihan komparasi dan logika

---
+++ latihanKomparasiDanLogika.py
@@ -0,0 +1,38 @@
+# episode latihan logika dan komparasi
+
+#membuat gabungan area rentang dari angka
+
+#++++++3--------10+++++++
+
+inputUser = float(input("masukan angka yang bernilai \n3 < angka > 10 : "))
+
+
+#++++++3------------------
+#memeriksa angka kurang dari 3
+isKurangDari = (inputUser) < 3
+
+#=============10++++++++++
+#memeriksa angka lebih dari 10
+isLebihDari = (inputUser) > 10
+
+#++++++3--------10+++++++
+#memeriksa angka diantara 3 dan 10
+isCorect = isKurangDari or isLebihDari
+print("angka yang anda masukan adalah : ", isCorect)
+
+#++++++3--------10+++++++
+#kasus irisan
+inputUser = float(input("masukan angka yang bernilai \n3 <  dan angka < 10 : "))
+
+#======3+++++++++++++++
+#memeriksa angka lebih dari 3
+isLebihDari = (inputUser) > 3
+
+#++++++++++++++10======
+#memeriksa angka kurang dari 10
+isKurangDari = (inputUser) < 10
+
+#======3+++++++++++++++10======
+#memeriksa angka diantara 3 dan 10
+isCorect = isLebihDari and isKurangDari
+print("angka yang anda masukan adalah : ", isCorect)
