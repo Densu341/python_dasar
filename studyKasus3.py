9011cc18b02a7c4b15add02f42fac003d5f64546
Deni Irawan <denyirawan341@gmail.com>
2023-01-18 22:45:31 +0700

    program menghitung jumlah keramik yang diperlukan untuk ruangan

--- studyKasus3.py
+++ studyKasus3.py
@@ -7,11 +7,14 @@ lRuang = int(input("Masukan Lebar ruangan yang ingin dipasang keramik dalam CM :
 
 pKeramik = int(input("Masukan Panjang keramik yang akan digunakan dalam CM : "))
 lKeramik = int(input("Masukan Lebar keramik yang akan digunakan dalam CM : "))
+jKeramik = int(input("Masukan Jumlah Keramik dalam 1 kotak : "))
 
 #rumus
 luasTotalRuang = (pRuang*lRuang)*ruang
 luasKeramik = (pKeramik*lKeramik)
 result = (luasTotalRuang/luasKeramik)
+box = (result/jKeramik)
 
 #output
-print("Jumlah Keramik yang diperlukan untuk %i Ruangan ialah %0.2f Keramik dengan ukuran %i cm X %i cm"%(ruang, result, pKeramik, lKeramik))
+print("-------------Hasil Perhitungan-------------")
+print("Jumlah Keramik yang diperlukan untuk %i Ruangan ialah %0.2f Keramik atau %i Kotak/Box dengan ukuran %i cm X %i cm"%(ruang, result, box, pKeramik, lKeramik))
