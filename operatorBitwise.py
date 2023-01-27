e0e10d74c95612be6c7845cb19d7fd7665da42e9
Deni Irawan <denyirawan341@gmail.com>
2023-01-28 00:07:43 +0700

    operator Bitwise

---
+++ operatorBitwise.py
@@ -0,0 +1,10 @@
+a = 9
+b = 5
+
+#bitwise OR(|)
+c = a | b
+print('nilai :',a,'binary',format(a,'08b'))
+print('nilai :',b,'binary',format(b,'08b'))
+print('nilai :',c,'binary',format(c,'08b'))
+
+#bitwise AND(&)
