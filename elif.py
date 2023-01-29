7e1aa0daf40aef621d18ec8049ce3f72e246d40f
Deni Irawan <denyirawan341@gmail.com>
2023-01-30 00:41:01 +0700

    episode if, else, elif

---
+++ elif.py
@@ -0,0 +1,13 @@
+# elif = else if
+
+nama = input("Siapa nama kamu? ")
+
+if nama == "otong":
+    print("Halo Otong manis!")
+elif nama == "ucup":
+    print("Halo Ucup ganteng!")
+elif nama == "mario":
+    print("Halo Mario keren!")
+else:
+    print("Terimakasih", nama,"!")
+print("Program selesai")
---
+++ ifDanElse.py
@@ -0,0 +1,26 @@
+#mengatur flow dari program dengan if dan else statement
+
+#1. if nya
+#2. kondisinya
+#3. aksinya
+
+nama = input("Siapa nama kamu? ")
+
+#program sebelumnya
+# if kondisi : aksi
+#program selanjutnya
+
+# 1. if inline
+# if nama == "Rizky" : print("Halo Rizky ganteng abiiiiiezz!!!!")
+# print(f"Halo {nama} ganteng")
+
+# 2. program if dengan indentasi
+# if nama == "Rizky" :
+#     print("Halo Rizky ganteng abiiiiiezz!!!!")
+# print(f"Terimakasih {nama} ganteng")
+
+# 3. if else statement
+if nama == "otong":
+    print("Halo otong ganteng abiiiiiezz!!!!")
+else:
+    print(f"Terimakasih {nama} ganteng")
